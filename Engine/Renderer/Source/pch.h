7cc48014332bd514d9968e4c153e68e0e962a886
Faustino Zanetto <faustizanetto@gmail.com>
2022-09-10 19:40:31 -0300

    Added basic source files.

---
+++ Engine/Renderer/Source/pch.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <iostream>
+#include <array>
+#include <string>
+#include <fstream>
+#include <random>
+#include <filesystem>
+#include <chrono>
+#include <thread>
+#include <vector>
+#include <algorithm>
+#include <sstream>
+#include <array>
+#include <vector>
+
+#include <string>
+#include <sstream>
+#include <array>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+#include <stdint.h>
+#include <cstdint>
+#include <cstdlib>
+#include <minmax.h>
+
+#ifndef NOMINMAX
+#define NOMINMAX // For windows.h
+#endif
+#include <Windows.h>
--- premake5.lua
+++ premake5.lua
@@ -8,8 +8,15 @@ workspace "Retro"
     "Release",
   }
 
-project "Engine"
-  location "Engine"
+outputDir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+IncludeDir = {}
+IncludeDir["GLFW"] = "%{wks.location}/ThirdParty/GLFW/include"
+IncludeDir["GLAD"] = "%{wks.location}/ThirdParty/GLAD/include"
+IncludeDir["GLM"] = "%{wks.location}/ThirdParty/GLM"
+
+project "EngineCore"
+  location "Engine/Core"
   kind "SharedLib"
   language "C++"
   cppdialect "C++20"
@@ -19,15 +26,54 @@ project "Engine"
   objdir ("Intermediates/" .. outputDir .. "/%{prj.name}")
 
   pchheader "pch.h"
-  pchsource "Engine/Source/pch.cpp"
+  pchsource "Engine/Core/Source/pch.cpp"
 
   files {
-    "%{prj.name}/Source/**.h",
-    "%{prj.name}/Source/**.cpp"
+    "Engine/Core/Source/**.h",
+    "Engine/Core/Source/**.cpp"
   }
 
   includedirs {
     "%{prj.name}/Source"
+  }
+
+  filter "system:windows"
+		cppdialect "C++latest"
+		systemversion "latest"
+
+  filter "configurations:Debug"
+    symbols "on"
+    optimize "Off"
+
+  filter "configurations:release"
+    optimize "Speed"   
+    
+  filter "configurations:Dist"
+		optimize "Full"
+
+project "EngineRenderer"
+  location "Engine/Renderer"
+  kind "SharedLib"
+  language "C++"
+  cppdialect "C++20"
+  staticruntime "on"
+
+  targetdir ("Binaries/" .. outputDir .. "/%{prj.name}")
+  objdir ("Intermediates/" .. outputDir .. "/%{prj.name}")
+
+  pchheader "pch.h"
+  pchsource "Engine/Renderer/Source/pch.cpp"
+
+  files {
+    "Engine/Renderer/Source/**.h",
+    "Engine/Renderer/Source/**.cpp"
+  }
+
+  includedirs {
+    "%{prj.name}/Source",
+    "%{IncludeDir.GLFW}",
+    "%{IncludeDir.GLAD}",
+    "%{IncludeDir.GLM}",
   }
 
   filter "system:windows"
