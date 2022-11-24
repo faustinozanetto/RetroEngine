ae3b31d09f7fe463f37a218bea20bbf622edd85b
Faustino Zanetto <faustizanetto@gmail.com>
2022-11-24 11:28:06 -0300

    Finally got PCSS Shadows working and fully implemented

--- Engine/Renderer/Source/renderer/renderer/scene_renderer.h
+++ Engine/Renderer/Source/renderer/renderer/scene_renderer.h
@@ -20,101 +20,108 @@
 
 namespace retro::renderer
 {
-    struct camera_data
-    {
-        glm::mat4 u_ViewProjectionMatrix;
-        glm::mat4 u_ViewMatrix;
-        glm::mat4 u_ProjectionMatrix;
-        glm::vec3 u_Position;
-    };
+	struct camera_data
+	{
+		glm::mat4 u_ViewProjectionMatrix;
+		glm::mat4 u_ViewMatrix;
+		glm::mat4 u_ProjectionMatrix;
+		glm::vec3 u_Position;
+	};
 
-    struct point_light_data
-    {
-        glm::vec3 position;
-        glm::vec3 color;
-        float intensity;
-        float radius;
-    };
+	struct point_light_data
+	{
+		glm::vec4 position;
+		glm::vec4 color;
+		float intensity;
+		float radius;
+	};
 
-    struct directional_light_data
-    {
-        glm::vec3 direction;
-        glm::vec3 color;
-        float intensity;
-    };
-    
-    struct csm_shadows
-    {
-        float m_avg_frustum_size;
-        float m_cascade_splits[NUM_CASCADES];
-        float m_light_radius_uv;
-        
-        std::vector<glm::mat4> m_dir_light_view_projection_matrices;
-        std::vector<glm::mat4> m_dir_light_view_matrices;
+	struct directional_light_data
+	{
+		glm::vec4 direction;
+		glm::vec4 color;
+		float intensity;
+	};
 
-        glm::uvec2 m_dir_light_shadow_map_res;
-        glm::vec2 m_dir_shadow_frustum_planes[NUM_CASCADES];
+	struct csm_shadows
+	{
+		int m_blocker_search_samples = 128;
+		int m_pcf_filter_samples = 128;
+		float m_light_radius_uv;
 
-        GLuint m_shadow_fbo;
-        GLuint m_dir_shadow_maps;
-        GLuint m_random_angles_tex3d_id;
-        GLuint m_pcf_sampler;
-    };
+		glm::vec2 scene_size;
+		glm::uvec2 m_dir_light_shadow_map_res;
+		glm::mat4 m_dir_light_view_projection;
+		glm::mat4 m_dir_light_view;
+		glm::vec2  m_dir_shadow_frustum_planes;
+		float      m_dir_shadow_frustum_size;
 
-    struct scene_renderer_data
-    {
-        camera_data m_camera_data;
-        point_light_data m_pointLight;
-        directional_light_data m_directional_light;
-        shared<camera> m_camera;
-        shared<scene> m_scene;
-        shared<uniform_buffer> m_camera_ubo;
-        shared<uniform_buffer> m_lights_ubo;
-        shared<uniform_buffer> shadows_ubo;;
-        shared<shader> m_geometry_shader;
-        shared<shader> m_lighting_shader;
-        shared<shader> m_shadow_shader;
-        shared<shader> m_screen_shader;
-        shared<shader> m_csm_shadows_shader;
-        shared<frame_buffer> m_geometry_frame_buffer;
-        shared<frame_buffer> m_shadow_frame_buffer;
-        shared<frame_buffer> m_final_frame_buffer;
-        shared<vertex_array_buffer> m_screen_vao;
-        shared<lighting_environment> m_lighting_environment;
-        shared<shadow_map> m_shadow_map;
-        csm_shadows m_csm_shadows;
-    };
+		GLuint m_dir_shadow_map;
+		GLuint m_random_angles_tex3d_id;
+		GLuint m_shadow_fbo;
+		GLuint m_pcf_sampler;
+	};
 
-    class scene_renderer
-    {
-    public:
-        static void initialize(const shared<camera>& camera);
+	struct shadows_data
+	{
+		glm::mat4 light_view_projection;
+		glm::mat4 light_view;
+	};
 
-        static void begin_render();
-        static void end_render();
-        static void set_scene(const shared<scene>& scene);
+	struct scene_renderer_data
+	{
+		camera_data m_camera_data;
+		point_light_data m_pointLight;
+		directional_light_data m_directional_light;
+		shared<camera> m_camera;
+		shared<scene> m_scene;
+		shared<uniform_buffer> m_camera_ubo;
+		shared<uniform_buffer> m_lights_ubo;
+		shared<uniform_buffer> shadows_ubo;
+		shared<shader> m_geometry_shader;
+		shared<shader> m_lighting_shader;
+		shared<shader> m_shadow_shader;
+		shared<shader> m_screen_shader;
+		shared<shader> m_csm_shadows_shader;
+		shared<frame_buffer> m_geometry_frame_buffer;
+		shared<frame_buffer> m_shadow_frame_buffer;
+		shared<frame_buffer> m_final_frame_buffer;
+		shared<vertex_array_buffer> m_screen_vao;
+		shared<lighting_environment> m_lighting_environment;
+		shared<shadow_map> m_shadow_map;
+		csm_shadows m_csm_shadows;
+		shadows_data shadows_data;
+	};
 
-        static shared<frame_buffer>& get_geometry_frame_buffer();
-        static GLuint get_depth_frame_buffer();
-        static shared<frame_buffer>& get_final_frame_buffer();
-        static shared<lighting_environment>& get_lighting_environment();
-        static shared<camera>& get_camera();
-        static scene_renderer_data& get_data();
-        static uint32_t get_final_texture();
+	class scene_renderer
+	{
+	public:
+		static void initialize(const shared<camera>& camera);
 
-        static void update_csm_splits();
-        static void update_csm_frusta();
-        
-        static void create_shadow_fbo(uint32_t width, uint32_t height);
-        static void generate_shadow_map(uint32_t width, uint32_t height);
-        static GLuint generate_random_angles_texture_3d(uint32_t size);
+		static void begin_render();
+		static void end_render();
+		static void set_scene(const shared<scene>& scene);
 
-    private:
-        static void load_shaders();
-        static void generate_frame_buffers();
-        static void create_screen_vao();
-        static void create_camera(const shared<camera>& camera);
-        static void setup_lights();
-        static void setup_environment();
-    };
+		static shared<frame_buffer>& get_geometry_frame_buffer();
+		static GLuint get_depth_frame_buffer();
+		static shared<frame_buffer>& get_final_frame_buffer();
+		static shared<lighting_environment>& get_lighting_environment();
+		static shared<camera>& get_camera();
+		static scene_renderer_data& get_data();
+		static uint32_t get_final_texture();
+
+		static void create_directional_shadow_map(uint32_t width, uint32_t height);
+		static void create_shadow_fbo(GLuint shadow_texture);
+		static void generate_shadow_map(uint32_t width, uint32_t height);
+		static void update_light_matrices();
+		static GLuint generate_random_angles_texture_3d(uint32_t size);
+
+	private:
+		static void load_shaders();
+		static void generate_frame_buffers();
+		static void create_screen_vao();
+		static void create_camera(const shared<camera>& camera);
+		static void setup_lights();
+		static void setup_environment();
+	};
 }
